b8aeec7bf5d7bb92f070064a6e1a52a4bbf3c2d7
Jordi Marti <jordimarti@me.com>
2013-12-31 10:17:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testenergia::Application.config.secret_key_base = '69e5a9dbb8e09144976fd1d37420403b1266406825061795e888a3fc732b95e120efc862d13d5b291185a42865fb89189def28777223bf91dd266d43de4a7f48'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testenergia::Application.config.session_store :cookie_store, key: '_testenergia_session'
