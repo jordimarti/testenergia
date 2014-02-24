5f6b40a017eea0e2dbbb68ecbb5a867c519f15ba
Jordi Martí Muñoz <jordimarti@me.com>
2014-02-24 10:04:45 +0100

    Changed config->environments->production.rb file to precompile woff files for glyphicon fonts from bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Testenergia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 end
