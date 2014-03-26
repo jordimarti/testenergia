cedc357fb6c54a8b6dd6c7016556b9081a819f31
Jordi Martí Muñoz <jordimarti@me.com>
2014-03-26 09:22:57 +0100

    Admin dashboard improvements

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -24,6 +24,16 @@ class AdminController < ApplicationController
     end
   end
 
+  def delete
+    @expert = Expert.find(params[:id])
+  end
+
+  def destroy
+    expert = Expert.find(params[:id]).destroy
+    flash[:notice] = "L'expert '#{expert.email}' ha estat correctament eliminat."
+    redirect_to(:action => 'index')
+  end
+
   private
     def expert_params
       params.require(:expert).permit(:expert_id, :expert_first_name, :expert_surname1, :expert_surname2, :expert_id_document_type, :expert_id_document_number, :expert_telephone, :expert_mobile_phone, :expert_address, :expert_zip_code, :expert_town, :expert_province, :title, :referee_number)
