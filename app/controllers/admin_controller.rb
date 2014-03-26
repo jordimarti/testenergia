e11797df36648d18339e8d21dd599e55b35846cb
Jordi Martí Muñoz <jordimarti@me.com>
2014-03-26 08:45:44 +0100

    Admin dashboard improvements

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  before_filter :authenticate_admin!
+
   def index
   	@experts = Expert.find(:all, :order => "id")
   	@xprojects = Xproject.find(:all)
